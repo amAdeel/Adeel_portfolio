ba52a02728146672b7d073daac4876a22874aae3
amAdeel <adeeleducationpurpose@gmail.com>
2024-04-17 22:29:46 +0500

    3rd commit

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -5,45 +5,49 @@ import { NAV_LINKS } from "../../../constants"
 import Button from "./Button"
 import React, { useState } from "react"
 
-const navbar = () => {
-  // const [navbarOpen, SetNavbarOpen]=useState(false);
+const Navbar = () => {
+  const [navbar, setNavbar] = useState(false);
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-3 text-white bg-[#2f4f4f]">
-      <Link href="\">
-        <Image src="/adeel website logo.jpeg" alt="logo" width={74} height={29} className="rounded-full " /> 
+      <Link href="/">
+        <Image src="/adeel website logo.jpeg" alt="logo" width={74} height={29} className="rounded-full" /> 
       </Link>
-      {/* <div className="mobile-menu block md:hidden">
-        { 
-        navbarOpen ?(
-          <button></button>
-        ):(
-          <button></button>
-        )
-        }
-      </div> */}
-      <ul className=" h-full lg:gap-12 flex sm:gap-5">
+      <div className="mobile-menu flex md:hidden ">
+        <button onClick={() => setNavbar(!navbar)} >
+          {navbar ? (
+            <Image src="/cross.svg" width={32} height={32} alt="logo" className=" cursor-pointer lg:hidden flex    pl-[300px] " />
+          ) : (
+            <Image 
+              src="/menu.svg"
+              alt="menu"
+              width={32}
+              height={32}
+              className=" cursor-pointer lg:hidden"
+            />
+          )}
+        </button>
+      </div>
+      <div className={`flex-1  justify-self-center md:block ${navbar ? 'sm:flexCenter md:p-0 ' : 'hidden'}`}>
+        <ul className="lg:flexCenter lg:h-full lg:gap-12 lg:flex sm:gap-5">
           {NAV_LINKS.map((link) => (
-            <Link href={link.href} key={link.key} className="regular-16 text-white flexCenter cursor-pointer pb-1.5  transition-all hover:font-bold">
+            <Link href={link.href} key={link.key} onClick={() => setNavbar(!navbar)} className="regular-16 text-white flexCenter cursor-pointer pb-1.5  transition-all hover:font-bold ">
               {link.label}
             </Link>
           ))}
         </ul>
-        <div className="lg:flexCenter ">
-          <Link href="/login">
+      </div>
+      <div className="lg:flexCenter hidden">
+        <Link href="/login" >
           <Button 
-          type="button"
-          title="Login"
-          icon ="/user.svg"
-          variant="btn_dark_green"
+            type="button"
+            title="Login"
+            icon="/user.svg"
+            variant="btn_dark_green"
           />
-          </Link>
-        </div>
-        
-
-  
+        </Link>
+      </div>
     </nav>
   )
- }
+}
 
-
-export default navbar
+export default Navbar
