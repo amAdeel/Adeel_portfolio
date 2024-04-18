916eae4f1f6a40ec8126f17bc996533161698340
amAdeel <adeeleducationpurpose@gmail.com>
2024-04-18 20:13:29 +0500

    4th commit

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -12,7 +12,14 @@ const Navbar = () => {
       <Link href="/">
         <Image src="/adeel website logo.jpeg" alt="logo" width={74} height={29} className="rounded-full" /> 
       </Link>
-      <div className="mobile-menu flex md:hidden ">
+      <ul className=" h-full flex flex-row  lg:gap-12   md:gap-12 md:flexCenter md:flex  xs:gap-6 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="lg:text-[20px] text-white flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold xs:text-[15px] xl:text-[40px] ">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      {/* <div className="mobile-menu flex md:hidden ">
         <button onClick={() => setNavbar(!navbar)} >
           {navbar ? (
             <Image src="/cross.svg" width={32} height={32} alt="logo" className=" cursor-pointer lg:hidden flex    pl-[300px] " />
@@ -27,15 +34,15 @@ const Navbar = () => {
           )}
         </button>
       </div>
-      <div className={`flex-1  justify-self-center md:block ${navbar ? 'sm:flexCenter md:p-0 ' : 'hidden'}`}>
-        <ul className="lg:flexCenter lg:h-full lg:gap-12 lg:flex sm:gap-5">
+      <div className={`flex-1  justify-self-center md:block ${navbar ? ' md:p-0 ' : 'hidden'}`}>
+        <ul className="lg:flexCenter lg:h-full lg:gap-12 lg:flex sm:gap-5 flex flex-row flexCenter pr-5 mr-10">
           {NAV_LINKS.map((link) => (
             <Link href={link.href} key={link.key} onClick={() => setNavbar(!navbar)} className="regular-16 text-white flexCenter cursor-pointer pb-1.5  transition-all hover:font-bold ">
               {link.label}
             </Link>
           ))}
-        </ul>
-      </div>
+        </ul> */}
+      {/* </div> */}
       <div className="lg:flexCenter hidden">
         <Link href="/login" >
           <Button 
@@ -46,6 +53,13 @@ const Navbar = () => {
           />
         </Link>
       </div>
+      <Image 
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className=" cursor-pointer hidden"
+      />
     </nav>
   )
 }
